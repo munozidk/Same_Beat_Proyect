211e82cbdc9c16046ceca5e5bf0e32f820d438d4
sarahurt <1110368984@u.icesi.edu.com>
2026-04-01 12:52:06 -0500

    fix:The chats were being duplicated three times, but there should only be three; I’ve fixed that now. We also changed some data variables and had to update the app.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-//render matches
-function renderMatches(usuarios) {
+function renderMatches(users) {
   const container = document.getElementById("matchesContainer");
 
   const staticCard = container.querySelector(".add-card");
@@ -8,15 +6,15 @@ function renderMatches(usuarios) {
   container.innerHTML = "";
   container.appendChild(staticCard);
 
-  usuarios.forEach(user => {
+  users.forEach(user => {
     const card = document.createElement("article");
     card.classList.add("match-card", "user-card");
 
     card.innerHTML = `
-      <img src="${user.imagen}" alt="${user.username}" class="match-pic">
+      <img src="${user.image}" alt="${user.username}" class="match-pic">
       <div class="match-info">
         <h3>${user.username}</h3>
-        <p>${user.compatibilidad}</p>
+        <p>${user.compatibility}</p>
       </div>
     `;
 
@@ -24,27 +22,25 @@ function renderMatches(usuarios) {
   });
 }
 
-//render posts
 function renderPost(post) {
   return `
     <article class="feed-post">
 
       <div class="post-header">
         <div class="author-info">
-          <img src="${post.imagen}" alt="${post.usuario}" class="comment-profile-pic">
-          <span class="author-name">${post.usuario}</span>
+          <img src="${post.image}" alt="${post.user}" class="comment-profile-pic">
+          <span class="author-name">${post.user}</span>
         </div>
       </div>
 
       <div class="post-content">
-        <p>${post.texto}</p>
+        <p>${post.text}</p>
       </div>
 
     </article>
   `;
 }
 
-//render para todos los posts
 function renderAllPosts(posts) {
   const container = document.getElementById("feedSection");
 
@@ -54,7 +50,7 @@ function renderAllPosts(posts) {
     container.innerHTML += renderPost(post);
   });
 }
-//render para los chats 
+
 function renderChats(chats) {
   const container = document.getElementById("chatList");
 
@@ -65,25 +61,18 @@ function renderChats(chats) {
     chatItem.classList.add("chat-item");
 
     chatItem.innerHTML = `
-      <img src="${chat.imagen}" alt="${chat.nombre}" class="chat-item__avatar">
-      <span class="chat-item__name">${chat.nombre}</span>
+      <img src="${chat.image}" alt="${chat.name}" class="chat-item__avatar">
+      <span class="chat-item__name">${chat.name}</span>
     `;
 
     container.appendChild(chatItem);
   });
 }
 
-
-
-
-// init conectandolo con el data
 document.addEventListener("DOMContentLoaded", () => {
-  renderMatches(usuarios);
+  renderMatches(users);
   renderAllPosts(posts);
-  
 });
-
-//like function 
 
 function handleLike (){
     const likeBtn = document.querySelectorAll(".like-btn");
@@ -102,8 +91,6 @@ function handleLike (){
     });
 }
 
-//Search function
-
 function handleSearch(posts) {
     const input = document.getElementById("searchInput");
 
@@ -116,11 +103,9 @@ function handleSearch(posts) {
 
         renderAllPosts(filteredPosts);
 
-        handleLike(); // Reattach like event listeners after rendering new posts (Esto es para reactivar los eventos)
+        handleLike();
     });
 }
-
-//Modal function
 
 function openModal(){
     document.getElementById("modalOverlay").style.display = "flex";
@@ -139,8 +124,6 @@ function handleModal(){
     if (sidebarBtn) sidebarBtn.addEventListener("click", openModal);
     if (closeBtn) closeBtn.addEventListener("click", closeModal);
 }
-
-// Create posts function
 
 function handleCreatePost(){
     const submitBtn = document.getElementById("modalSubmitBtn");
@@ -167,11 +150,10 @@ function handleCreatePost(){
     })
 }
 
-//INIT Final
-
 function initApp() {
     renderMatches(MATCHES);
     renderAllPosts(POSTS);
+    renderChats(chats);
 
     handleLike();
     handleSearch(POSTS);
@@ -180,4 +162,3 @@ function initApp() {
 }
 
 initApp();
-
